14807c89ed13bdffcb1f5c850fbbc72575c6291c
pwarias <93276356+PiotrWarias@users.noreply.github.com>
2022-03-29 11:26:23 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Main {
+  public static void main (String[] args)
+  {
+    int x = 10;   
+    for(int i=1;i<=x;i++)
+      {
+        for(int j=1;j<=x;j++)
+          {
+            System.out.print(j*i+ " ");
+          }
+        System.out.println("");
+      }
+  }
+}
